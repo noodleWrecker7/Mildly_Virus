1e148348d42214c34ee41891584ec32890f29ee5
hodgk <hodgkinson.adam@gmail.com>
2021-01-21 11:53:33 +0000

    performance improvements?

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,31 @@ import java.net.URL;
 
 public class Main {
 
+    static URL imageUrl = ClassLoader.getSystemResource("troll.png");
+    static ImageIcon image = new ImageIcon(imageUrl);
+
     public static void main(String args[]) {
-        //for (int i = 0; i < 10; i++) {
-            while(true) {
+//        for (int i = 0; i < 5; i++) {
+        while (true) {
+            threadage thread1 = new threadage();
+            thread1.start();
+        }
+       /* for (int i = 0; i < 10; i++) {
+            //while(true) {
+
+        }*/
+    }
+}
+
+class threadage extends Thread {
+
+    @Override
+    public void run() {
+//        for (int i = 0; i < 20; i++) {
+        while (true) {
             JDialog frame = new JDialog();
             frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
-            URL imageUrl = ClassLoader.getSystemResource("troll.png");
-            ImageIcon image = new ImageIcon(imageUrl);
-            JLabel label = new JLabel(image);
-            frame.add(label);
+            frame.add(new JLabel(Main.image));
             frame.pack();
             frame.setVisible(true);
             frame.setLocation((int) Math.floor(Math.random() * 1920), (int) Math.floor(Math.random() * 1080));
